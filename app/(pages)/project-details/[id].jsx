e5dc3880c6d8722b85ead46577e6b54bc159a177
Sardar Bilal <hussnainraja768@gmail.com>
2024-06-02 23:40:08 +0500

    Bilal

--- app/(pages)/project-details/[id].jsx
+++ app/(pages)/project-details/[id].jsx
@@ -7,27 +7,40 @@ import Paragraph from "@/app/ui/paragraph/Paragraph";
 import Image from "next/image";
 import React from "react";
 
-const page = ({ searchParams }) => {
-  const idFromSearchParams = parseInt(searchParams.id);
-  const projectToShowDetails = projects.filter(
-    (project) => project.id === idFromSearchParams
-  );
+// getStaticPaths function to generate paths at build time
+export async function getStaticPaths() {
+  const paths = projects.map((project) => ({
+    params: { id: project.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
+
+// getStaticProps function to fetch project details based on the id
+export async function getStaticProps({ params }) {
+  const id = parseInt(params.id);
+  const projectToShowDetails = projects.find((project) => project.id === id);
+
+  return { props: { projectToShowDetails } };
+}
+
+const ProjectDetails = ({ projectToShowDetails }) => {
   return (
     <div className="bg-lightGray w-full">
       <div className="bg-black min-h-dvh">
         <MaxWidthWrapper className={" pt-20 md:pt-36 px-5  "}>
-          <WorkItem hasButton={false} index={1} el={projectToShowDetails[0]} />
+          <WorkItem hasButton={false} index={1} el={projectToShowDetails} />
         </MaxWidthWrapper>
       </div>
       <MaxWidthWrapper className="py-20  ">
         <div className="pb-10 px-5">
           <H1 className="text-center">About the project</H1>
           <Paragraph className="text-center w-full md:w-3/4 m-auto ">
-            {projectToShowDetails[0].projectCompanyandTechInfo}
+            {projectToShowDetails.projectCompanyandTechInfo}
           </Paragraph>
         </div>
         <div className="flex flex-col gap-20">
-          {projectToShowDetails[0].extraInfo.map((item, index) => (
+          {projectToShowDetails.extraInfo.map((item, index) => (
             <ImageTextLayout
               text={item.detail}
               image={item.image}
@@ -37,11 +50,11 @@ const page = ({ searchParams }) => {
           ))}
         </div>
         <Paragraph className="text-center w-[95%] md:w-3/4 m-auto pt-20 ">
-          {projectToShowDetails[0].projectWorking}
+          {projectToShowDetails.projectWorking}
         </Paragraph>
       </MaxWidthWrapper>
     </div>
   );
 };
 
-export default page;
+export default ProjectDetails;
